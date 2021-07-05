b7e4cd89099c710e8ac0193eff607249d7f5ddea
TheCodex6824 <thecodex6824@gmail.com>
2021-07-05 19:01:32 -0400

    Fix elytra cape transformer being broken, again

--- src/main/java/thecodex6824/thaumicaugmentation/core/transformer/TransformerRenderCape.java
+++ src/main/java/thecodex6824/thaumicaugmentation/core/transformer/TransformerRenderCape.java
@@ -20,6 +20,7 @@
 
 package thecodex6824.thaumicaugmentation.core.transformer;
 
+import org.objectweb.asm.Label;
 import org.objectweb.asm.Opcodes;
 import org.objectweb.asm.Type;
 import org.objectweb.asm.tree.AbstractInsnNode;
@@ -36,7 +37,7 @@ public class TransformerRenderCape  extends Transformer {
     
     @Override
     public boolean needToComputeFrames() {
-        return false;
+        return true;
     }
     
     @Override
@@ -56,18 +57,40 @@ public class TransformerRenderCape  extends Transformer {
                     Type.VOID_TYPE, Type.getType("Lnet/minecraft/client/entity/AbstractClientPlayer;"), Type.FLOAT_TYPE, Type.FLOAT_TYPE, Type.FLOAT_TYPE, Type.FLOAT_TYPE, Type.FLOAT_TYPE, Type.FLOAT_TYPE, Type.FLOAT_TYPE),
                     "(Lnet/minecraft/client/entity/AbstractClientPlayer;FFFFFFF)V");
             
-            int check = TransformUtil.findFirstInstanceOfOpcode(render, 0, Opcodes.IF_ACMPEQ);
+            int check = TransformUtil.findFirstField(render, 0, TransformUtil.remapFieldName("net/minecraft/init/Items", "field_185160_cR"),
+                    "Lnet/minecraft/item/Item;", "net/minecraft/init/Items");
             if (check != -1) {
-                AbstractInsnNode insertAfter = render.instructions.get(check);
-                LabelNode label = ((JumpInsnNode) insertAfter).label;
-                render.instructions.insert(insertAfter, new JumpInsnNode(Opcodes.IFEQ, label));
-                render.instructions.insert(insertAfter, new MethodInsnNode(Opcodes.INVOKESTATIC,
-                        TransformUtil.HOOKS_CLIENT,
-                        "shouldRenderCape",
-                        "(Lnet/minecraft/client/entity/AbstractClientPlayer;)Z",
-                        false
-                ));
-                render.instructions.insert(insertAfter, new VarInsnNode(Opcodes.ALOAD, 1));
+                // apparently the checks/jumps are inverted outside dev (?)
+                if (render.instructions.get(check).getNext().getOpcode() == Opcodes.IF_ACMPEQ) {
+                    AbstractInsnNode insertAfter = render.instructions.get(check).getNext();
+                    LabelNode label = ((JumpInsnNode) insertAfter).label;
+                    render.instructions.insert(insertAfter, new JumpInsnNode(Opcodes.IFEQ, label));
+                    render.instructions.insert(insertAfter, new MethodInsnNode(Opcodes.INVOKESTATIC,
+                            TransformUtil.HOOKS_CLIENT,
+                            "shouldRenderCape",
+                            "(Lnet/minecraft/client/entity/AbstractClientPlayer;)Z",
+                            false
+                    ));
+                    render.instructions.insert(insertAfter, new VarInsnNode(Opcodes.ALOAD, 1));
+                }
+                else {
+                    AbstractInsnNode insertAfter = render.instructions.get(check).getPrevious().getPrevious().getPrevious();
+                    LabelNode newLabel = new LabelNode(new Label());
+                    render.instructions.insert(insertAfter, new JumpInsnNode(Opcodes.IFEQ, newLabel));
+                    render.instructions.insert(insertAfter, new MethodInsnNode(Opcodes.INVOKESTATIC,
+                            TransformUtil.HOOKS_CLIENT,
+                            "shouldRenderCape",
+                            "(Lnet/minecraft/client/entity/AbstractClientPlayer;)Z",
+                            false
+                    ));
+                    render.instructions.insert(insertAfter, new VarInsnNode(Opcodes.ALOAD, 1));
+                    
+                    check = TransformUtil.findFirstInstanceOfOpcode(render, check, Opcodes.RETURN);
+                    if (check != -1)
+                        render.instructions.insert(render.instructions.get(check).getPrevious(), newLabel);
+                    else
+                        throw new TransformerException("Could not locate required instructions, complex case");
+                }
             }
             else
                 throw new TransformerException("Could not locate required instructions");
