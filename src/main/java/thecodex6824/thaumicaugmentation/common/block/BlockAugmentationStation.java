931239d4fbdd7bc1bf5c7eee8120a56a92db9dde
TheCodex6824 <thecodex6824@gmail.com>
2022-04-30 13:53:27 -0400

    Fixed augmentation station model not working

--- src/main/java/thecodex6824/thaumicaugmentation/common/block/BlockAugmentationStation.java
+++ src/main/java/thecodex6824/thaumicaugmentation/common/block/BlockAugmentationStation.java
@@ -25,23 +25,18 @@ import net.minecraft.block.material.Material;
 import net.minecraft.block.state.BlockFaceShape;
 import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.util.EnumBlockRenderType;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.Mirror;
-import net.minecraft.util.NonNullList;
 import net.minecraft.util.Rotation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 import thecodex6824.thaumicaugmentation.ThaumicAugmentation;
-import thecodex6824.thaumicaugmentation.api.TAItems;
 import thecodex6824.thaumicaugmentation.api.block.property.IHorizontallyDirectionalBlock;
 import thecodex6824.thaumicaugmentation.common.block.prefab.BlockTABase;
 import thecodex6824.thaumicaugmentation.common.block.trait.IItemBlockProvider;
@@ -57,7 +52,6 @@ public class BlockAugmentationStation extends BlockTABase implements IItemBlockP
 	
 	@Override
 	protected BlockStateContainer createBlockState() {
-		// TODO Auto-generated method stub
 		return new BlockStateContainer(this, IHorizontallyDirectionalBlock.DIRECTION);
 	}
 	
@@ -76,9 +70,19 @@ public class BlockAugmentationStation extends BlockTABase implements IItemBlockP
     public IBlockState getStateForPlacement(World world, BlockPos pos, EnumFacing facing, float hitX, float hitY,
             float hitZ, int meta, EntityLivingBase placer, EnumHand hand) {
 
-        return getDefaultState().withProperty(IHorizontallyDirectionalBlock.DIRECTION, placer.getHorizontalFacing().getOpposite());
+        return super.getStateForPlacement(world, pos, facing, hitX, hitY, hitZ, meta, placer, hand)
+                .withProperty(IHorizontallyDirectionalBlock.DIRECTION, placer.getHorizontalFacing().getOpposite());
     }
 
+	@Override
+    public IBlockState withRotation(IBlockState state, Rotation rot) {
+        return state.withProperty(IHorizontallyDirectionalBlock.DIRECTION, rot.rotate(state.getValue(IHorizontallyDirectionalBlock.DIRECTION)));
+    }
+
+    @Override
+    public IBlockState withMirror(IBlockState state, Mirror mirror) {
+        return state.withRotation(mirror.toRotation(state.getValue(IHorizontallyDirectionalBlock.DIRECTION)));
+    }
 
 	@Override
 	public boolean onBlockActivated(World world, BlockPos pos, IBlockState state, EntityPlayer player,
@@ -100,58 +104,48 @@ public class BlockAugmentationStation extends BlockTABase implements IItemBlockP
 	
 	@Override
 	public boolean canHarvestBlock(IBlockAccess world, BlockPos pos, EntityPlayer player) {
-		// TODO Auto-generated method stub
 		return true;
 	}
 	
 	@Override
 	public boolean isFullBlock(IBlockState state) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public boolean isFullCube(IBlockState state) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public boolean isTopSolid(IBlockState state) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 	
 	@Override
 	public boolean isOpaqueCube(IBlockState state) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 	
 	@Override
 	public boolean isNormalCube(IBlockState state) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 	
 	@Override
 	public BlockFaceShape getBlockFaceShape(IBlockAccess worldIn, IBlockState state, BlockPos pos, EnumFacing face) {
-		// TODO Auto-generated method stub
 		return BlockFaceShape.UNDEFINED;
 	}
 
 	@Override
 	public BlockRenderLayer getRenderLayer() {
-		// TODO Auto-generated method stub
 		return BlockRenderLayer.SOLID;
 	}
 	
 	@Override
 	public EnumBlockRenderType getRenderType(IBlockState state) {
-		// TODO Auto-generated method stub
 		return EnumBlockRenderType.MODEL;
 	}
-	
 	
 }
 
