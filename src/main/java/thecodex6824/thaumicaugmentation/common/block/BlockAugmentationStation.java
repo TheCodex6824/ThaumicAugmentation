3ce226620c56553459c362e9d4afb274d19a93cc
WillPastor <47420496+WillPastor@users.noreply.github.com>
2022-04-30 13:09:48 -0400

    Updated Augmentation Station methods and blockstate.

--- src/main/java/thecodex6824/thaumicaugmentation/common/block/BlockAugmentationStation.java
+++ src/main/java/thecodex6824/thaumicaugmentation/common/block/BlockAugmentationStation.java
@@ -23,26 +23,63 @@ package thecodex6824.thaumicaugmentation.common.block;
 
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.BlockFaceShape;
+import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.util.EnumBlockRenderType;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
+import net.minecraft.util.Mirror;
+import net.minecraft.util.NonNullList;
+import net.minecraft.util.Rotation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 import thecodex6824.thaumicaugmentation.ThaumicAugmentation;
+import thecodex6824.thaumicaugmentation.api.TAItems;
+import thecodex6824.thaumicaugmentation.api.block.property.IHorizontallyDirectionalBlock;
 import thecodex6824.thaumicaugmentation.common.block.prefab.BlockTABase;
 import thecodex6824.thaumicaugmentation.common.block.trait.IItemBlockProvider;
+import thecodex6824.thaumicaugmentation.common.util.BitUtil;
 import thecodex6824.thaumicaugmentation.init.GUIHandler;
 
-public class BlockAugmentationStation extends BlockTABase implements IItemBlockProvider {
+public class BlockAugmentationStation extends BlockTABase implements IItemBlockProvider, IHorizontallyDirectionalBlock {
 	
 	public BlockAugmentationStation() {
 		super(Material.WOOD);
+		setDefaultState(getDefaultState().withProperty(IHorizontallyDirectionalBlock.DIRECTION, EnumFacing.NORTH));
 	}
 	
+	@Override
+	protected BlockStateContainer createBlockState() {
+		// TODO Auto-generated method stub
+		return new BlockStateContainer(this, IHorizontallyDirectionalBlock.DIRECTION);
+	}
+	
+	
+	@Override
+	public IBlockState getStateFromMeta(int meta) {
+		return getDefaultState().withProperty(IHorizontallyDirectionalBlock.DIRECTION, EnumFacing.byHorizontalIndex(BitUtil.getBits(meta, 0, 2)));
+	}
+
+	@Override
+	public int getMetaFromState(IBlockState state) {
+		return BitUtil.setBits(0, 0, 2, state.getValue(IHorizontallyDirectionalBlock.DIRECTION).getHorizontalIndex());
+	}
+	
+	@Override
+    public IBlockState getStateForPlacement(World world, BlockPos pos, EnumFacing facing, float hitX, float hitY,
+            float hitZ, int meta, EntityLivingBase placer, EnumHand hand) {
+
+        return getDefaultState().withProperty(IHorizontallyDirectionalBlock.DIRECTION, placer.getHorizontalFacing().getOpposite());
+    }
+
+
 	@Override
 	public boolean onBlockActivated(World world, BlockPos pos, IBlockState state, EntityPlayer player,
 	        EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
@@ -59,6 +96,7 @@ public class BlockAugmentationStation extends BlockTABase implements IItemBlockP
 
         return true;
 	}
+	
 	
 	@Override
 	public boolean canHarvestBlock(IBlockAccess world, BlockPos pos, EntityPlayer player) {
@@ -113,6 +151,7 @@ public class BlockAugmentationStation extends BlockTABase implements IItemBlockP
 		// TODO Auto-generated method stub
 		return EnumBlockRenderType.MODEL;
 	}
+	
 	
 }
 
